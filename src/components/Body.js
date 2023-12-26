38fbac4be96a18df28ed91477bf562797f32f897
arnab24nov <arnabkhanrah11@gmail.com>
2023-12-27 00:19:00 +0530

    useEffect hooks, fetch real API and rending the UI, shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,38 @@
 import RestaurentCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 import { useState } from "react";
+import { useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.769599&lng=73.865613&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    // return (
+    //   <div className="rendering-loader">
+    //     <div className="loader"></div>
+    //     <h1>Loading...</h1>
+    //   </div>
+    // );
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <button
         className="filter-btn"
         onClick={() => {
-          console.log("button clicked");
           setListOfRestaurants(
             listOfRestaurants.filter((el) => el.info.avgRating > 4.0)
           );
@@ -23,7 +46,20 @@ const Body = () => {
           placeholder="search for restaurants"
           id="search-box"
         />
-        <input id="search-button" type="button" value="Search" />
+        <input
+          id="search-button"
+          type="button"
+          value="Search"
+          onClick={() => {
+            console.log("button clicked");
+            let searchItem = document.getElementById("search-box").value;
+            setListOfRestaurants(
+              listOfRestaurants.filter((el) =>
+                el.info.cuisines.includes(searchItem)
+              )
+            );
+          }}
+        />
       </div>
       <div className="res-container">
         <div className="card-container">
